094eee5a340006ea14c2359db3d2993cf8275e77
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-04-01 22:26:31 +0530

    final commit

---
+++ Extention/content.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+    const style = document.createElement('style');
+    style.innerHTML = '*:hover { display:none !important; }';
+    document.head.appendChild(style);
+  });
+  
+
+
+// ©️
+//   ___         __ __    _  __     ___        __            
+//   / _ \ __ __ / // /__ (_)/ /_   / _ )__ __ / /_ ____ ___ _
+//  / ___// // // //  '_// // __/  / _  |\ \ // __// __// _ `/
+// /_/    \_,_//_//_/\_\/_/ \__/  /____//_\_\ \__//_/   \_,_/ 
